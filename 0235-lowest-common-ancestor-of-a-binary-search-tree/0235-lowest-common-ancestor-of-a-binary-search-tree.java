ecf18d067591d7cada2ea432c4cb34ed4918b818
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-03-09 13:17:27 +0530

    Time: 6 ms (38.43%), Space: 44.5 MB (94.97%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // if the root node is in the range of the root's left and right child's node(1st example)
+        if(p.val < q.val)
+            if(p.val < root.val && q.val > root.val) return root;
+        if(p.val > q.val)
+            if(p.val > root.val && q.val < root.val) return root;
+        // if one of the parent node is 'p' or 'q' and one of the child node is the remaining among 'p' or 'q'(2nd example) 
+        if(root.val == p.val || root.val == q.val){
+            if(root.val == p.val && ((root.left != null && root.left.val == q.val) || (root.right != null && root.right.val == q.val))) return p;
+            else return q;
+        }
+        // else the sub-trees are checked according to the BST rules
+        if(p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+        else return lowestCommonAncestor(root.right, p, q);
+    }
+}
