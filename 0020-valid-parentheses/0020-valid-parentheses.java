e5b0fdabb88e969fd69f04ae7a59d3e18f5bcd1e
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-04-22 21:58:17 +0530

    Time: 2 ms (54.63%), Space: 41.1 MB (92.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                if(s.charAt(i) == '(')
+                    stk.push(')');
+                else if(s.charAt(i) == '{')
+                    stk.push('}');
+                else if(s.charAt(i) == '[')
+                    stk.push(']');
+            }
+            else if(stk.isEmpty() || stk.peek() != s.charAt(i)) 
+                return false;
+            else stk.pop();
+        }
+        return stk.isEmpty();
+    }
+}
+/*class Solution {
+    public boolean isValid(String s) {
         char arr[] = new char[s.length()];
         int f=0,p=-1;
         for(int i=0;i<s.length();i++){
@@ -28,4 +47,4 @@ class Solution {
         else
             return false;        
     }
-}
+}*/
