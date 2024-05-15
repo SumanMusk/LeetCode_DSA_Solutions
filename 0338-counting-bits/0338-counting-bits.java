dff5d7cfafe589d9430c854e723e6b532ecbd55b
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-05-15 13:07:05 +0530

    Time: 2 ms (95.05%), Space: 46.7 MB (29.95%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] countBits(int n) {
+        int dp[] = new int[n+1];
+        int offset=1;
+        for(int i=1;i<=n;i++) {
+            if(offset * 2 == i)
+                offset = i;
+            dp[i] = 1 + dp[i - offset];
+        }
+        return dp;
+    }
+}
