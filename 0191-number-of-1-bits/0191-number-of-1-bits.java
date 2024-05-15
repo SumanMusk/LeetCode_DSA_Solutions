8f0837c8c4baa2997b24b8bbeab7bb28eddcd66a
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-05-15 12:35:41 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (88.31%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n != 0) {
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+    }
+}
+/*
+// Traversing each bit and checking each last bit is 1 or not
+// One of Bit-Manipulation Pattern Question
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n != 0) {
+            count += n & 1;
+            n = n >> 1;
+        }
+        return count;
+    }
+}
+// Converting Decimal to Binary number by Wrapper Class methods (without using any bit-manipulation)
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        String s = Integer.toBinaryString(n);
+        for(char ch: s.toCharArray())
+            if(ch == '1')
+                count++;
+        return count;
+    }
+}
+*/
