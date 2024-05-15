6c8b836d18ba0bdd0adf253bd1d02202ca755cfb
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-05-15 12:36:34 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (49.89%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,22 +1,22 @@
-class Solution {
-    public int hammingWeight(int n) {
-        int count=0;
-        while(n != 0) {
-            n = n & (n-1);
-            count++;
-        }
-        return count;
-    }
-}
-/*
-// Traversing each bit and checking each last bit is 1 or not
 // One of Bit-Manipulation Pattern Question
+// Traversing each bit and checking each last bit is 1 or not
 class Solution {
     public int hammingWeight(int n) {
         int count=0;
         while(n != 0) {
             count += n & 1;
             n = n >> 1;
+        }
+        return count;
+    }
+}
+/*
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n != 0) {
+            n = n & (n-1);
+            count++;
         }
         return count;
     }
