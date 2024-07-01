3ce22664d78669a7c83085a2dda0264c29fe63d0
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-01 20:26:36 +0530

    Time: 0 ms (100%), Space: 42 MB (57.07%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.java
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -1,7 +1,19 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        int f=0;
-        List<Integer> ls = new ArrayList<>();
+        int c=0;
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]%2 != 0) {
+                c++;
+                if(c == 3)
+                    return true;
+            }
+            else c=0;
+        }
+        return false;
+    }
+}
+/*class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
         for(int i=0;i<arr.length;i++) {
             if(arr[i]%2 != 0 && i+2 < arr.length) 
                 if(arr[i+1]%2 != 0 && arr[i+2]%2 != 0)
@@ -9,4 +21,4 @@ class Solution {
         }
         return false;
     }
-}
+}*/
