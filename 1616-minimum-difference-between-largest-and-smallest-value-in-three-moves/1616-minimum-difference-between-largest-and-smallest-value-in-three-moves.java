d65ae11fec364b2c66cf8ba2ff6d851a7767ce31
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-03 18:50:55 +0530

    Time: 24 ms (12.86%), Space: 54.9 MB (95.28%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -2,11 +2,34 @@ class Solution {
     public int minDifference(int[] nums) {
         if(nums.length <= 4)
             return 0;
-        Arrays.sort(nums);
-        int ans=Integer.MAX_VALUE, i=0, j=nums.length-4;
-        for(int x=0;x<4;x++) {
-            ans = Math.min(ans, nums[j++]-nums[i++]);
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        List<Integer> maxOnes = new ArrayList<>();
+        List<Integer> minOnes = new ArrayList<>();
+        for(int i: nums) {
+            minHeap.add(i);
+            maxHeap.add(i);
         }
+        for(int x=0;x<4;x++) {
+            maxOnes.add(maxHeap.poll());
+            minOnes.add(minHeap.poll());
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int x=1;x<=4;x++) 
+            ans = Math.min(ans, maxOnes.get(4-x)-minOnes.get(x-1));
         return ans;
     }
 }
+/*
+class Solution {
+    public int minDifference(int[] nums) {
+        if(nums.length <= 4)
+            return 0;
+        Arrays.sort(nums);
+        int ans=Integer.MAX_VALUE, i=0, j=nums.length-4;
+        for(int x=0;x<4;x++) 
+            ans = Math.min(ans, nums[j++]-nums[i++]);
+        return ans;
+    }
+}
+*/
