fed7bde99967607f25bed6c4e3da7d96cbc0e88e
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-09 00:04:44 +0530

    Time: 48 ms (8.34%), Space: 44.5 MB (7.81%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -1,5 +1,21 @@
 class Solution {
     public int findTheWinner(int n, int k) {
+        Queue<Integer> que = new LinkedList<>();
+        for(int i=1;i<=n;i++)
+            que.add(i);
+        while(que.size() > 1) {
+            for(int i=1;i<=k-1;i++) {
+                int temp = que.poll();
+                que.add(temp);
+            }
+            que.poll();
+        }
+        return que.poll();
+    }
+}
+// Brute Force
+/*class Solution {
+    public int findTheWinner(int n, int k) {
         List<Integer> arr = new ArrayList<>(){{for(int i=1;i<=n;i++) add(i);}};
         int idx=0;
         while(arr.size() > 1) {
@@ -14,4 +30,4 @@ class Solution {
         }
         return arr.get(0);
     }
-}
+}*/
