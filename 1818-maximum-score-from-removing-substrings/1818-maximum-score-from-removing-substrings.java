867c4fbe52901affee9fc7e24dd7f8c7eb028291
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-12 18:37:12 +0530

    Time: 140 ms (57.58%), Space: 46.3 MB (48.48%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -3,16 +3,15 @@ class Solution {
         String chkStr = (x < y) ? "ba" : "ab";
         int res = 0, cNum = (x < y) ? y : x;
         Stack<Character> stk = new Stack<>();
-        res = helper(s, stk, chkStr, cNum, res);
+        res = helper(s, stk, chkStr, cNum, 0);
         cNum = (x+y)-cNum;
         chkStr = (chkStr.equals("ba")) ? "ab" : "ba";
         s = new StringBuilder(stk.stream().map(String::valueOf).collect(Collectors.joining())).toString();
-        if(s.length() == 0)
-            return res;
         stk.clear();
-        return helper(s, stk, chkStr, cNum, res);
+        return res + helper(s, stk, chkStr, cNum, 0);
     }
     public int helper(String s, Stack<Character> stk, String chkStr, int x, int res) {
+        if(s.length() == 0) return 0;
         stk.push(s.charAt(0));
         for(int i=1;i<s.length();i++) {
             if(s.charAt(i) == chkStr.charAt(1) && !stk.isEmpty() && stk.peek() == chkStr.charAt(0)) {
