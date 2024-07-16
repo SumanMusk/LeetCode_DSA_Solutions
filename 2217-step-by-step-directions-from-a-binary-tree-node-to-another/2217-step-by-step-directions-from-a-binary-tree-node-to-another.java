d30efa53e96813bd3b884143afb177e4d37776ef
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-16 22:23:26 +0530

    Time: 59 ms (18.66%), Space: 94.9 MB (7.53%) - LeetHub

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.java
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -2,32 +2,33 @@ class Solution {
     public String getDirections(TreeNode root, int startValue, int destValue) {
         String startPath = path(root, startValue, new ArrayList<>());
         String destPath = path(root, destValue, new ArrayList<>());
-        int i=0;
-        while(i < Math.min(startPath.length(), destPath.length())) {
-            if(startPath.charAt(i) != destPath.charAt(i))
+        int i=0, j=0;
+        while(i < startPath.length() && j < destPath.length()) {
+            if(startPath.charAt(i) != destPath.charAt(j))
                 break;
             i++;
+            j++;
         }
-        return startPath.substring(i, startPath.length()).replaceAll(".", "U") + destPath.substring(i, destPath.length());
+        return startPath.substring(i, startPath.length()).replaceAll(".", "U") + destPath.substring(j, destPath.length());
     }
 
     public String path(TreeNode root, int destVal, List<Character> ls) {
         if(root == null) 
             return "";
-        if(root.val == destVal) {
+        if(root.val == destVal) 
             return new StringBuilder(ls.stream().map(String::valueOf).collect(Collectors.joining())).toString();
-        }
+        
         ls.add('L');
         String resOne = path(root.left, destVal, ls);
-        // if(!resOne.equals(""))
-        if(resOne.length() != 0)
+        if(!resOne.equals(""))
             return resOne;
+        
         ls.removeLast();
         ls.add('R');
         String resTwo = path(root.right, destVal, ls);
-        // if(!resTwo.equals(""))
-        if(resTwo.length() != 0)
+        if(!resTwo.equals(""))
             return resTwo;
+        
         ls.removeLast();
         return "";
     }
