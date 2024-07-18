8f3ed1710fc0897201b95b7697f11be6dd403dae
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-18 20:54:24 +0530

    Time: 25 ms (40.34%), Space: 44.7 MB (55.42%) - LeetHub

---
+++ 1653-number-of-good-leaf-nodes-pairs/1653-number-of-good-leaf-nodes-pairs.java
@@ -0,0 +1,43 @@
+class Solution {
+    int count=0;
+    public int countPairs(TreeNode root, int distance) {
+        dfs(root, distance);
+        return count;
+    }
+    public List<Integer> dfs(TreeNode root, int distance) {
+        if(root.left == null && root.right == null) 
+            return new ArrayList<>(Arrays.asList(1));
+        List<Integer> leftList;
+        List<Integer> rightList;
+        leftList = (root.left == null) ? new ArrayList<>() : dfs(root.left, distance);
+        rightList = (root.right == null) ? new ArrayList<>() : dfs(root.right, distance);
+        if(leftList.size() == 0)
+            return increment(rightList);
+        if(rightList.size() == 0)
+            return increment(leftList);
+        if(leftList.size() <= rightList.size())
+            chkDist(leftList, rightList, distance);
+        if(leftList.size() > rightList.size())
+            chkDist(rightList, leftList, distance);
+        return increment(leftList, rightList);
+    }
+    public void chkDist(List<Integer> small, List<Integer> big, int dist) {
+        for(int i: small)
+            for(int j: big)
+                if(i+j <= dist)
+                    count++;
+    }
+    public List<Integer> increment(List<Integer> ls) {
+        for(int i=0;i<ls.size();i++)
+            ls.set(i, ls.get(i)+1);
+        return ls;
+    }
+    public List<Integer> increment(List<Integer> one, List<Integer> two) {
+        List<Integer> res = new ArrayList<>();
+        for(int i: one)
+            res.add(i+1);
+        for(int i: two)
+            res.add(i+1);
+        return res;
+    }
+}
