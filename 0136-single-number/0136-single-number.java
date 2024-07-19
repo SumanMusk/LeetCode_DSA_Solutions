bc69d63591bbe81fc8ba0a4b7a746ae22f4200b8
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-19 17:54:50 +0530

    Time: 1 ms (99.89%), Space: 46.1 MB (26.03%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,9 @@
+// Bit-Manipulation pattern Question: XOR Operation
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor=nums[0];
+        for(int i=1;i<nums.length;i++)
+            xor = xor ^ nums[i];
+        return xor;
+    }
+}
