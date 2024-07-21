38dc667142d3c8667a9790b50c7594fe7421ea12
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-21 20:13:01 +0530

    Time: 243 ms (5.11%), Space: 46.4 MB (6.89%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -10,7 +10,7 @@ class Solution {
         Set<Integer> visited = new HashSet<>();
         for(int i=0;i<numCourses;i++) 
             if(!visited.contains(i))
-                if(!isCyclic(i, adjLs, visited, topoQue))
+                if(!isCyclic(i, adjLs, visited))
                     return new int[]{};
         visited.clear();
         for(int i=0;i<numCourses;i++) 
@@ -22,12 +22,12 @@ class Solution {
             res[c++] = topoQue.poll();
         return res;
     }
-    public boolean isCyclic(int curr, Map<Integer, List<Integer>> adjLs, Set<Integer> visited, Queue<Integer> topoQue) {
+    public boolean isCyclic(int curr, Map<Integer, List<Integer>> adjLs, Set<Integer> visited) {
         if(visited.contains(curr))
             return false;
         visited.add(curr);
         for(int i=0;i<adjLs.get(curr).size();i++)
-            if(!isCyclic(adjLs.get(curr).get(i), adjLs, visited, topoQue))
+            if(!isCyclic(adjLs.get(curr).get(i), adjLs, visited))
                 return false;
         visited.remove(curr);
         return true;
@@ -41,3 +41,39 @@ class Solution {
         topoQue.offer(curr);
     }
 }
+/*
+// Merged the helperDFS and isCyclic method into only one dfs method, but for the contains method for inserting elements in topoQue, we're getting time limit exceeded error
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adjLs = new HashMap<>();
+        for(int i=0;i<numCourses;i++) 
+            adjLs.put(i, new ArrayList<>());
+        for(int i=0;i<prerequisites.length;i++) 
+            adjLs.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        // Generally we use a Stack for Topological Sorting, but for this case(for the answer array order) we're using a Queue 
+        Queue<Integer> topoQue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        for(int i=0;i<numCourses;i++) 
+            if(!visited.contains(i))
+                if(helperDFS(i, adjLs, visited, topoQue))
+                    return new int[]{};
+        int res[] = new int[topoQue.size()];
+        int c=0;
+        while(!topoQue.isEmpty())
+            res[c++] = topoQue.poll();
+        return res;
+    }
+    public boolean helperDFS(int curr, Map<Integer, List<Integer>> adjLs, Set<Integer> visited, Queue<Integer> topoQue) {
+        if(visited.contains(curr))
+            return true;
+        visited.add(curr);
+        for(int i=0;i<adjLs.get(curr).size();i++)
+            if(helperDFS(adjLs.get(curr).get(i), adjLs, visited, topoQue))
+                return true;
+        visited.remove(curr);
+        if(!topoQue.contains(curr))
+            topoQue.offer(curr);
+        return false;
+    }
+}
+*/
