71fced3903ec4a6428ad04e267e6d4ac347f12de
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-22 18:32:54 +0530

    Time: 16 ms (37.31%), Space: 55.2 MB (80.6%) - LeetHub

--- 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.java
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.java
@@ -1,66 +1,64 @@
 class Solution {
-    public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
-        List<Integer> row_sorting = topo_sort(rowConditions, k);
-        List<Integer> col_sorting = topo_sort(colConditions, k);
-        if (row_sorting.isEmpty() || col_sorting.isEmpty())
+    public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {        
+        List<Integer> sortedRow = func(rowConditions, k);
+        List<Integer> sortedCol = func(colConditions, k);
+        if(sortedRow.size() == 0 || sortedCol.size() == 0)       
             return new int[0][0];
 
-        Map<Integer, int[]> value_position = new HashMap<>();
-        for (int n = 1; n <= k; ++n) {
-            value_position.put(n, new int[2]);  // element -> [row_index, col_index]
-        }
-        for (int ind = 0; ind < row_sorting.size(); ++ind) {
-            value_position.get(row_sorting.get(ind))[0] = ind;
-        }
-        for (int ind = 0; ind < col_sorting.size(); ++ind) {
-            value_position.get(col_sorting.get(ind))[1] = ind;
-        }
-
-        int[][] res = new int[k][k];
-        for (int value = 1; value <= k; ++value) {
-            int row = value_position.get(value)[0];
-            int column = value_position.get(value)[1];
-            res[row][column] = value;
-        }
-
-        return res;
+        Map<Integer, int[]> map = new HashMap<>();
+        int[][] mat = new int[k][k];
+        for(int x=1;x<=k;x++) 
+            map.put(x, new int[2]);
+        for(int x=0;x<sortedRow.size();x++) 
+            map.get(sortedRow.get(x))[0] = x;
+        for(int x=0;x<sortedCol.size();x++) 
+            map.get(sortedCol.get(x))[1] = x;
+        for(int x=1;x<=k;x++) 
+            mat[map.get(x)[0]][map.get(x)[1]] = x;
+        return mat;
     }
-
-    // return True if all okay and return False if cycle was found
-    private boolean dfs(int src, Map<Integer, List<Integer>> graph, Set<Integer> visited, Set<Integer> cur_path, List<Integer> res) {
-        if (cur_path.contains(src)) return false;  // cycle detected
-        if (visited.contains(src)) return true;  // all okay, but we've already visited this node
-
-        visited.add(src);
-        cur_path.add(src);
-
-        for (int neighbor : graph.getOrDefault(src, Collections.emptyList())) {
-            if (!dfs(neighbor, graph, visited, cur_path, res))  // if any child returns false
+    public List<Integer> func(int[][] condi, int k) {
+        Map<Integer, List<Integer>> adjLs = new HashMap<>();
+        int indegree[] = new int[k+1];
+        for(int i=1;i<=k;i++)
+            adjLs.put(i, new ArrayList<>());
+        for(int i=0;i<condi.length;i++) {
+            int pre = condi[i][0];
+            int post = condi[i][1];
+            List<Integer> ls = adjLs.get(pre);
+            ls.add(post);
+            adjLs.put(pre, ls);
+            indegree[post]++;
+        }
+        Queue<Integer> topoQue = new LinkedList<>();
+        List<Integer> ls = new ArrayList<>();
+        for(int i=1;i<indegree.length;i++) 
+            if(indegree[i] == 0)
+                topoQue.add(i);
+        while(!topoQue.isEmpty()) {
+            int node = topoQue.poll();
+            ls.add(node);
+            for(Integer i: adjLs.get(node)) {
+                indegree[i]--;
+                if(indegree[i] == 0)
+                    topoQue.add(i);
+            }
+        }  
+        if(ls.size() == k)
+            return ls;
+        return new ArrayList<>();
+    }
+    public boolean isCyclic(int i, List<List<Integer>> ls, Set<Integer> visited, Stack<Integer> stk) {
+        if(visited.contains(i))
+            return false;
+        visited.add(i);
+        for(int x=0;x<ls.get(i).size();x++) {
+            if(!isCyclic(ls.get(i).get(x), ls, visited, stk))
                 return false;
         }
-
-        cur_path.remove(src);  // backtrack path
-        res.add(src);
+        visited.remove(i);
+        if(!stk.contains(i))
+            stk.push(i);
         return true;
-    }
-
-    // if there will be cycle - return empty array, in other case return 1d array as described above
-    private List<Integer> topo_sort(int[][] edges, int k) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int[] edge : edges) {
-            graph.computeIfAbsent(edge[0], x -> new ArrayList<>()).add(edge[1]);
-        }
-
-        Set<Integer> visited = new HashSet<>();
-        Set<Integer> cur_path = new HashSet<>();
-        List<Integer> res = new ArrayList<>();
-
-        for (int src = 1; src <= k; ++src) {
-            if (!dfs(src, graph, visited, cur_path, res))
-                return Collections.emptyList();
-        }
-
-        Collections.reverse(res);  // we will have res as reversed so we need to reverse it one more time
-        return res;
     }
 }
