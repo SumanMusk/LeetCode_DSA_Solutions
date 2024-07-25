757da95f678cb2201392f6aa1d1c4641148bbe24
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-25 21:37:11 +0530

    Time: 2497 ms (5.02%), Space: 55.8 MB (60.74%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        for(int i=0;i<nums.length-1;i++) {
-            for(int j=0;j<nums.length-i-1;j++) {
-                if(nums[j] > nums[j+1]) {
-                    int x = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = x;
-                }
+        for(int i=1;i<nums.length;i++) {
+            int curr = nums[i];
+            int j = i;
+            while(j > 0 && nums[j-1] > curr) {
+                nums[j] = nums[j-1];
+                j--;
             }
+            nums[j] = curr;
         }
         return nums;
     }
