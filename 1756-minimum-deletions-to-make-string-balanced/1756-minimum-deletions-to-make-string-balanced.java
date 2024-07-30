dd8c71e19d6eb3ec9ccf775fc9bcb00c4b5d2dcc
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-07-30 13:07:29 +0530

    Time: 44 ms (25.07%), Space: 45.3 MB (57.66%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
@@ -1,5 +1,23 @@
 class Solution {
     public int minimumDeletions(String s) {
+        int a_count=0;
+        for(int i=0;i<s.length();i++) 
+            if(s.charAt(i) == 'a')
+                a_count++;
+        int b_count_left=0, res=s.length();
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i) == 'a')
+                a_count--;
+            res = Math.min(res, (a_count + b_count_left));
+            if(s.charAt(i) == 'b')
+                b_count_left++;
+        }
+        return res;
+    }
+}
+/*
+class Solution {
+    public int minimumDeletions(String s) {
         int dp[] = new int[s.length()];
         for(int i=s.length()-2;i>=0;i--) 
             dp[i] = dp[i+1] + (s.charAt(i+1) == 'a' ? 1 : 0);
@@ -12,3 +30,4 @@ class Solution {
         return res;
     }
 }
+*/
