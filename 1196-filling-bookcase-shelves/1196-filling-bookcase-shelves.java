383258533e3c7d3edb6497f38b77e94de400784c
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-08-01 14:22:52 +0530

    Time: 1 ms (76.67%), Space: 48 MB (8.49%) - LeetHub

--- 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.java
+++ 1196-filling-bookcase-shelves/1196-filling-bookcase-shelves.java
@@ -1,19 +1,21 @@
 class Solution {
+    int mainW;
+    public int solve(int i, int currW, int maxH, int books[][], int[][] dp) {
+        if(i == books.length)
+            return maxH;
+        if(dp[i][currW] != 0)
+            return dp[i][currW];
+        int keep = Integer.MAX_VALUE;
+        int skip = Integer.MAX_VALUE;
+        if(books[i][0] <= currW)
+            keep = solve(i+1, currW-books[i][0], Math.max(maxH, books[i][1]), books, dp);
+        skip = maxH + solve(i+1, mainW-books[i][0], books[i][1], books, dp);
+        dp[i][currW] = Math.min(keep, skip);
+        return dp[i][currW];
+    }
     public int minHeightShelves(int[][] books, int shelfWidth) {
-        int n = books.length;
-        int[] f = new int[n + 1];
-        for (int i = 1; i <= n; ++i) {
-            int w = books[i - 1][0], h = books[i - 1][1];
-            f[i] = f[i - 1] + h;
-            for (int j = i - 1; j > 0; --j) {
-                w += books[j - 1][0];
-                if (w > shelfWidth) {
-                    break;
-                }
-                h = Math.max(h, books[j - 1][1]);
-                f[i] = Math.min(f[i], f[j - 1] + h);
-            }
-        }
-        return f[n];
+        int dp[][] = new int[books.length][shelfWidth+1];
+        mainW = shelfWidth;
+        return solve(0, shelfWidth, 0, books, dp);
     }
 }
