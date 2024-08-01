ac719e10fab4ee9cc29316007a84e2d529971171
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-08-01 12:00:56 +0530

    Time: 1 ms (82.64%), Space: 42.8 MB (21.29%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int c=0;
+        for(String s: details) {
+            if(Integer.parseInt(s.substring(11, 13)) > 60)
+                c++;
+        }
+        return c;
+    }
+}
