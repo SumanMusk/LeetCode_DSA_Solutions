7e4dfd0661ca59efc4901845a16f5709ec55bcd7
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-08-04 17:23:21 +0530

    Time: 65 ms (72.52%), Space: 46.2 MB (58.56%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
@@ -1,3 +1,4 @@
+// Using int array
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
         int arr[] = new int[(n*(n+1))/2];
@@ -15,3 +16,24 @@ class Solution {
         return sum;
     }
 }
+
+/*
+// Using an ArrayList
+class Solution {
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        List<Integer> ls = new ArrayList<>();
+        for(int i=0;i<nums.length;i++) {
+            int sum=0;
+            for(int j=i;j<nums.length;j++) {
+                sum += nums[j];
+                ls.add(sum);
+            }
+        }
+        Collections.sort(ls);
+        int sum=0, mod=1000000007;
+        for(int i=left-1;i<right;i++) 
+            sum = (sum + ls.get(i)) % mod;
+        return sum;
+    }
+}
+*/
