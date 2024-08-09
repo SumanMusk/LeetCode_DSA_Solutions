6c6efa8fe0405a3dd45bf16312a27741205b608a
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-08-09 17:05:09 +0530

    Time: 1 ms (41.08%), Space: 41.6 MB (26.49%) - LeetHub

--- 0870-magic-squares-in-grid/0870-magic-squares-in-grid.java
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.java
@@ -1,25 +1,28 @@
 class Solution {
     public int numMagicSquaresInside(int[][] grid) {
         int c=0;
-        for(int i=0;i<grid.length-2;i++) 
-            for(int j=0;j<grid[0].length-2;j++) 
-                if(helper(grid, i, j))
-                    c++;
+        for(int i=0;i<grid.length;i++) 
+            for(int j=0;j<grid[0].length;j++) 
+                if(i+2 < grid.length && j+2 < grid[0].length)
+                    if(helper(grid, i, j))
+                        c++;
         return c;
     }
     public boolean helper(int[][] grid, int row, int col) {
-        Set<Integer> set = new HashSet<>();        
-        int c=0;
-        // Horizontally(rows)
+        Set<Integer> set = new HashSet<>();
         for(int i=row;i<=row+2;i++) {
             for(int j=col;j<=col+2;j++) {
-                if(grid[i][j] > 0 && grid[i][j] <= 9 && !set.contains(grid[i][j])) {
+                if(grid[i][j] > 0 && grid[i][j] <= 9 && !set.contains(grid[i][j])) 
                     set.add(grid[i][j]);
-                    c += grid[i][j];
-                }
                 else
                     return false;
             }
+        }
+        int c=0, sum=Integer.MAX_VALUE;
+        // Horizontally(rows)
+        for(int i=row;i<=row+2;i++) {
+            for(int j=col;j<=col+2;j++) 
+                c += grid[i][j];
             if(c != 15)
                 return false;
             c=0;
