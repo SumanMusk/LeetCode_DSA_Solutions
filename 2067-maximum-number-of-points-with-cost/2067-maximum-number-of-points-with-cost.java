1d3df22159877b157c7639049c65f64d33173dc7
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-08-18 00:19:22 +0530

    Time: 1665 ms (9.41%), Space: 77.1 MB (67.65%) - LeetHub

---
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.java
@@ -0,0 +1,61 @@
+class Solution {
+    public long maxPoints(int[][] points) {
+        long prev_Row[] = new long[points[0].length];
+        for(int i=0;i<points[0].length;i++) 
+            prev_Row[i] = points[0][i];
+        for(int i=1;i<points.length;i++) {
+            long[] tempArr = new long[points[0].length];
+            long[] left = new long[points[0].length];
+            long[] right = new long[points[0].length];
+
+            left[0] = prev_Row[0];
+            right[right.length-1] = prev_Row[prev_Row.length-1];
+            
+            for(int j=1;j<points[0].length;j++) 
+                left[j] = Math.max(left[j-1]-1, prev_Row[j]);
+            
+            for(int j=points[0].length-2;j>=0;j--) 
+                right[j] = Math.max(right[j+1]-1, prev_Row[j]);
+
+            for(int j=0;j<points[0].length;j++) 
+                tempArr[j] = points[i][j] + Math.max(left[j], right[j]);
+
+            prev_Row = Arrays.copyOf(tempArr, points[i].length);
+            for(long g: prev_Row)
+                System.out.print(g+" ");
+        }
+        long res=0;
+        for(long i: prev_Row) 
+            res = Math.max(res, i);
+        return res;
+    }
+}
+/*
+// Brute Force
+class Solution {
+    public long maxPoints(int[][] points) {
+        long prev_Row[] = new long[points[0].length];
+        for(int i=0;i<points[0].length;i++) 
+            prev_Row[i] = points[0][i];
+        for(int i=1;i<points.length;i++) {
+            long[] tempArr = new long[points[0].length];
+            for(int j=0;j<points[0].length;j++) {
+                long maxVal = Integer.MIN_VALUE;
+                long curr_point = points[i][j];
+                for(int k=0;k<prev_Row.length;k++) {
+                    long val = (long)(curr_point + prev_Row[k]) - (long)Math.abs(j-k);
+                    if(maxVal < val) {
+                        maxVal = val;
+                        tempArr[j] = val;
+                    }
+                }
+            }
+            prev_Row = Arrays.copyOf(tempArr, points[i].length);
+        }
+        long res=0;
+        for(long i: prev_Row) 
+            res = Math.max(res, i);
+        return res;
+    }
+}
+*/
