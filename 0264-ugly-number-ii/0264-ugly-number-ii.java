2417399d18efd0a13a5ee2ca14319e22c5ceff27
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-08-18 21:16:46 +0530

    Time: 53 ms (14.01%), Space: 44.8 MB (5.92%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
     public int nthUglyNumber(int n) {
         Queue<Long> minHeap = new PriorityQueue<>();
         minHeap.add((long)1);
@@ -23,8 +23,8 @@
         }
         return (int)res;
     }
-}*/
-
+}
+/*
 // Dp Approach
 class Solution {
     public int nthUglyNumber(int n) {
@@ -48,3 +48,4 @@ class Solution {
         return dp[n];
     }
 }
+*/
