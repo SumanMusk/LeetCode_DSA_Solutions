ad15b893d1caef0d499ccbded01139c1bfa11f43
SumanMusk <130691623+SumanMusk@users.noreply.github.com>
2024-08-19 17:31:44 +0530

    Time: 126 ms (5.12%), Space: 89.8 MB (5.24%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -2,17 +2,21 @@ class Solution {
     public int minSteps(int n) {
         if(n == 1) 
             return 0;
-        return 1 + helper(n, 1, 1);
+        int dp[][] = new int[2000][2000];
+        return 1 + helper(n, dp, 1, 1);
     }
-    public int helper(int n, int curr_A, int clipBoard_A) {
+    public int helper(int n, int[][] dp, int curr_A, int clipBoard_A) {
         if(curr_A == n)
             return 0;
+        if(dp[curr_A][clipBoard_A] != 0)
+            return dp[curr_A][clipBoard_A];
         if(curr_A > n)
             return 10000;
 
-        int copy_and_paste = 1 + 1 + helper(n, curr_A + curr_A, curr_A);
-        int only_paste = 1 + helper(n, curr_A + clipBoard_A, clipBoard_A);
+        int copy_and_paste = 1 + 1 + helper(n, dp, curr_A + curr_A, curr_A);
+        int only_paste = 1 + helper(n, dp, curr_A + clipBoard_A, clipBoard_A);
 
-        return Math.min(copy_and_paste, only_paste);
+        dp[curr_A][clipBoard_A] = Math.min(copy_and_paste, only_paste);
+        return dp[curr_A][clipBoard_A];
     }
 }
